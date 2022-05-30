56fa3e6b647a093f58f438ebd522adb7fda38fdb
Aestersa <borya.kaminskiy@gmail.com>
2022-05-30 20:14:05 +0200

    adding database connection

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-contoured-54157",
-    port: 5432,
-    user: "postgres",
-    password: "mu5Kolp91",
-    database: "smartbrain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
